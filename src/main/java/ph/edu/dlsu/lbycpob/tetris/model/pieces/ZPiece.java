7d6b1dafdc9f88c1bb07eccdb3d799896ff02950
DLSUJohnEzraMIguel <john_ezra_miguel_balili@dlsu.edu.ph>
2026-07-29 01:21:39 +0800

    Set default Z piece

--- src/main/java/ph/edu/dlsu/lbycpob/tetris/model/pieces/ZPiece.java
+++ src/main/java/ph/edu/dlsu/lbycpob/tetris/model/pieces/ZPiece.java
@@ -1,14 +1,20 @@
 package ph.edu.dlsu.lbycpob.tetris.model.pieces;
 
 public class ZPiece extends TetrominoBase {
-    /**
-     * Constructor - This runs when a new Tetris piece is created
-     *
-     * @param shapes - All the rotation patterns for this piece type
-     * @param color  - The color this piece should be displayed in
-     */
+
+    public ZPiece() {
+        super(
+                new int[][][] {
+                        { {1, 1, 0}, {0, 1, 1}, {0, 0, 0} },
+                        { {0, 0, 1}, {0, 1, 1}, {0, 1, 0} },
+                        { {0, 0, 0}, {1, 1, 0}, {0, 1, 1} },
+                        { {0, 1, 0}, {1, 1, 0}, {1, 0, 0} }
+                },
+                TetrominoColor.RED
+        );
+    }
+
     public ZPiece(int[][][] shapes, TetrominoColor color) {
         super(shapes, color);
     }
-    // TODO
 }
