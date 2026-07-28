269b2a5ec09efdff530768ddb3ad24324d4961a7
DLSUJohnEzraMIguel <john_ezra_miguel_balili@dlsu.edu.ph>
2026-07-29 00:32:26 +0800

    Set default J piece

--- src/main/java/ph/edu/dlsu/lbycpob/tetris/model/pieces/JPiece.java
+++ src/main/java/ph/edu/dlsu/lbycpob/tetris/model/pieces/JPiece.java
@@ -1,14 +1,20 @@
 package ph.edu.dlsu.lbycpob.tetris.model.pieces;
 
 public class JPiece extends TetrominoBase {
-    /**
-     * Constructor - This runs when a new Tetris piece is created
-     *
-     * @param shapes - All the rotation patterns for this piece type
-     * @param color  - The color this piece should be displayed in
-     */
+
+    public JPiece() {
+        super(
+                new int[][][] {
+                        { {1, 0, 0}, {1, 1, 1}, {0, 0, 0} },
+                        { {0, 1, 1}, {0, 1, 0}, {0, 1, 0} },
+                        { {0, 0, 0}, {1, 1, 1}, {0, 0, 1} },
+                        { {0, 1, 0}, {0, 1, 0}, {1, 1, 0} }
+                },
+                TetrominoColor.BLUE
+        );
+    }
+
     public JPiece(int[][][] shapes, TetrominoColor color) {
         super(shapes, color);
     }
-    // TODO
 }
