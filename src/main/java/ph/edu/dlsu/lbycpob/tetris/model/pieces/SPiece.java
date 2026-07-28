02fa305e1f85531e5483da50af6d1538b6ede924
DLSUJohnEzraMIguel <john_ezra_miguel_balili@dlsu.edu.ph>
2026-07-29 01:01:58 +0800

    Set default S piece

--- src/main/java/ph/edu/dlsu/lbycpob/tetris/model/pieces/SPiece.java
+++ src/main/java/ph/edu/dlsu/lbycpob/tetris/model/pieces/SPiece.java
@@ -1,14 +1,20 @@
 package ph.edu.dlsu.lbycpob.tetris.model.pieces;
 
 public class SPiece extends TetrominoBase {
-    /**
-     * Constructor - This runs when a new Tetris piece is created
-     *
-     * @param shapes - All the rotation patterns for this piece type
-     * @param color  - The color this piece should be displayed in
-     */
+
+    public SPiece() {
+        super(
+                new int[][][] {
+                        { {0, 1, 1}, {1, 1, 0}, {0, 0, 0} },
+                        { {0, 1, 0}, {0, 1, 1}, {0, 0, 1} },
+                        { {0, 0, 0}, {0, 1, 1}, {1, 1, 0} },
+                        { {1, 0, 0}, {1, 1, 0}, {0, 1, 0} }
+                },
+                TetrominoColor.GREEN
+        );
+    }
+
     public SPiece(int[][][] shapes, TetrominoColor color) {
         super(shapes, color);
     }
-    // TODO
 }
