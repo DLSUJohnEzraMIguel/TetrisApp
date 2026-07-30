0a40e9d50da3f8e45b08f3cd42e2715d1f29ac63
DLSUJohnEzraMIguel <john_ezra_miguel_balili@dlsu.edu.ph>
2026-07-30 22:24:53 +0800

    Set default Single piece

---
+++ src/main/java/ph/edu/dlsu/lbycpob/tetris/model/pieces/SinglePiece.java
@@ -0,0 +1,20 @@
+package ph.edu.dlsu.lbycpob.tetris.model.pieces;
+
+public class SinglePiece extends TetrominoBase {
+
+    public SinglePiece() {
+        super(
+                new int[][][]{
+                        // State 0 (Only 1 rotation needed for a 1x1 block)
+                        {
+                                {1}
+                        }
+                },
+                TetrominoColor.CYAN // Choose any color available in TetrominoColor
+        );
+    }
+
+    public SinglePiece(int[][][] shapes, TetrominoColor color) {
+        super(shapes, color);
+    }
+}
